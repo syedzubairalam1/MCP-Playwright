c36c4400bf2a8061eec8ac71fe475336738e7831
syedzubairalam1 <syed@gatherit.co>
2025-04-28 17:28:33 +0500

    Delete playwrightmcb directory

--- playwrightmcb/features/step_definitions/login.steps.js
+++
@@ -1,4 +0,0 @@
-Then('the text {string} should be visible', async function(text) {
-  const element = await page.getByText(text, { exact: true });
-  await expect(element).toBeVisible();
-}); 
--- playwrightmcb/generate-report.js
+++
@@ -1,5 +0,0 @@
-// Script to generate HTML report after tests are run
-const { generateReport } = require('./tests/support/reporting/reporter');
-
-// Generate the HTML report
-generateReport(); 
